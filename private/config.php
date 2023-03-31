d163132c3f1a886509f4756c929a01373a8d9071
norvin <norvin1107@gmail.com>
2023-03-31 11:04:20 +0800

    Update sa of 11:03 am 31 Mar 2023.

--- private/config.php
+++ private/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", 'p@$$w0rd');
+define("DB_PASSWORD", 'password123');
 define("DB_NAME", "webdevr10cs2gs");
 ?>
